8f1756c7f25552acc8f6f6dfa30721a9e4618edb
juliandunne1234 <julianmorandunne@gmail.com>
2023-02-20 17:47:33 +0000

    removed secret key and set debug

--- ssaw/settings.py
+++ ssaw/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-peel8h&fu*db&fhsxe8+2he2xj&#&k0elinc4xdz*lxj8jh1-b'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ssaw-collections.herokuapp.com', 'localhost']
 
